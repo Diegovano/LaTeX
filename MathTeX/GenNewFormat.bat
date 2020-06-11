36de5d5041c1ed8a14908ee4d552e5fbcded131e
Diegovano <36131808+Diegovano@users.noreply.github.com>
2020-06-11 16:52:12 +0100

    Added Maths for 11 June

--- MathTeX/GenNewFormat.bat
+++ MathTeX/GenNewFormat.bat
@@ -1,3 +1,3 @@
 @echo off
-pdflatex -ini -jobname="exerciseformat" -synctex=1 -interaction=nonstopmode --shell-escape "&pdflatex" mylatexformat.ltx """VariablesAleatoires.tex"""
+pdflatex -ini -jobname="exerciseformat" -synctex=1 -interaction=nonstopmode --shell-escape "&pdflatex" mylatexformat.ltx """Scalaire.tex"""
 pause
